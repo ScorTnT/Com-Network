9fdcd661f3eb68aa610ecb8577495c4fc68eab74
ScorTnT <flalxlemdkdlel@naver.com>
2024-10-12 16:41:07 +0900

    fin checkSum _v1, _v2

--- Assignment2/checkSum_v1.py
+++ Assignment2/checkSum_v1.py
@@ -3,6 +3,7 @@ def appendZero(a, b):
     for i in range(b):
         tmp += "0"
     a += int(tmp)
+
     return a
 
 def formToBit(polyno):
@@ -20,17 +21,18 @@ def formToBit(polyno):
             else:
                 tmp = int(i[1:])
             result = appendZero(result, tmp)
+
     return result
 
 def cal(a, b):
     org = b
+
     if len(b) > len(a):
         return a
     else:
         gap = len(a) - len(b)
         for i in range(gap):
             b+="0"
-    # print(a,b)
 
     result = ""
     for i in range(len(a)):
@@ -39,27 +41,17 @@ def cal(a, b):
         else:
             result += "0"
     result = result[result.find("1"):]
-    # print (result)
+
     return cal(result, org)
 
 def main():
-    # 1 0 0 1 0 1 0 1 1 0 0 1 1 1
-    # 1 0 1 1 1 0 0 0 0 
-    #   0 1 0 1 1 0 1 1 
-    #     1 0 1 1 1 0 0  
-    #     0 0 0 0 1 1 1 
-    #     1 0 1 0 0 0 
-    #         0 0 0 0
     commandA = "x+1+x2+x4" #input('다항식 입력')
-    commandB = "1001010" #input('데이터 입력')
+    commandB = "1110110110010" #input('데이터 입력')
     commandA = str(formToBit(commandA))
-    print(commandB, commandA)
     result = cal(commandB, commandA)
     for i in range(len(commandA)):
         commandB += "0"
-    # print(commandB, result)
     commandB = str(int(commandB) + int(result))
-    # print(commandB, result)
     print(commandB)
 
 if __name__=='__main__':
--- Assignment2/checkSum_v2.py
+++ Assignment2/checkSum_v2.py
@@ -1,5 +1,49 @@
+def cal(data, polyno):
+    data = format(data, 'b')
+    org = polyno = format(polyno, 'b')
+
+    if len(data) < len(polyno):
+        return data
+    else:
+        gap = len(data)-len(polyno)
+        polyno = int(polyno,2)
+        for i in range(gap):
+            polyno = polyno<<1
+    polyno = format(polyno,'b')
+
+    result = ""
+    for i in range(len(data)):
+        if data[i] != polyno[i]:
+            result += "1"
+        else:
+            result += "0"
+    
+    return cal(int(result[result.find("1"):],2),int(org,2))
+
+def formToint(polyno):
+    result = 0
+    polyno = polyno.split('+')
+
+    for i in polyno:
+        if 'x' in i:
+            if i == 'x':
+                result += 2
+            else:
+                result += 2**int(i[1:])
+        else:
+            result += 1
+
+    return result
+
 def main():
-    print()
+    commandA = "x+1+x2+x4" #input('다항식 입력')
+    commandB = int("1110110110010",2) #int(input('데이터 입력'),2)
+    commandA = formToint(commandA)
+    result= cal(data=commandB, polyno=commandA)
+    for i in range(len(format(commandA,'b'))):
+        commandB = commandB << 1
+    commandB += int(result,2)
+    print(format(commandB,'b'))
     
 if __name__=='__main__':
     main()
